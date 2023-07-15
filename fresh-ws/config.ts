f41683853cc2af6c2381399460a7073602fed278
Rob <kritz.rob@gmail.com>
2023-07-15 19:53:24 +0200

    update config

--- fresh-ws/config.ts
+++ fresh-ws/config.ts
@@ -1,5 +1,5 @@
 export const env =       Deno.env.get('FRESH_ENVIRONMENT')!;
 export const MONGO_URL = Deno.env.get("MONGO_URL")!;
-export const APP_URL =   Deno.env.get("DENO_APP_URL")!
+export const APP_URL =   Deno.env.get("APP_URL")!
 
 export const DB_NAME = "ts-ws";
